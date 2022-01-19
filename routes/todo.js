22e5ae00fe479c6745cd1cc96d0d2117c1446441
Irina <hallo.ween@mail.ru>
2022-01-19 16:09:32 +0300

    post query update

--- routes/todo.js
+++ routes/todo.js
@@ -1,20 +1,55 @@
-const {Router} = require('express');
+const {
+    Router
+} = require('express');
 const Todo = require('../models/todo');
 const router = Router();
 
 router.get('/', (req, res) => {
+    try {
 
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    try {
+        //console.log(req.body);
+        const todo = await Todo.create({
+            title: req.body.title,
+            done: false,
+        });
+        res.status(201).json({todo});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
 });
 
 router.put('/:id', (req, res) => {
+    try {
 
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
 });
 
 router.delete('/:id', (req, res) => {
+    try {
 
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Server error'
+        });
+    }
 });
 module.exports = router;
